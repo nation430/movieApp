89d3e1ea1b8ce27edf11734700335650fb587602
nation430 <tonyenation430@gmail.com>
2021-07-27 22:11:47 +0100

    another change

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,6 @@
 const API_URL = 'http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=bd854b491ffa8fa4b1f636d516756c6a&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=bd854b491ffa8fa4b1f636d516756c6a&query="'
 
 const main = document.getElementById('main')
 const form = document.getElementById('form')
